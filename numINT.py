9d2bcc57e22c7c28b22fd5ec19a2e4a3e09c2c70
Tristan <hollyt1@unlv.nevada.edu>
2025-10-26 12:22:24 -0700

    Update numINT.py

--- numINT.py
+++ numINT.py
@@ -28,5 +28,13 @@ from phonenumbers import geocoder, carrier
 parser = argparse.ArgumentParser(description='Phone number intelligence, powered by open source data\nhttps://github.com/T-Devs0/numINT.git', prog='numINT features', usage='%(prog)s -n <number>')
 parser.add_argument('--n', metavar ='number', type=str, help='Enter number to scan here')
 args = parser.parse_args()
-print(parser.description)
-parser.print_usage()
+
+if len(sys.argv) == 1:
+    print('\033[36m')
+    print(parser.description)
+    parser.print_usage()
+    print('\033[0m')
+
+#  function for number scan to call API here 
+#  def num_scan():
+
