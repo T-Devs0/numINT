8025aa9e84572e8673983bb15d0ee8d1333d8ee3
Tristan <hollyt1@unlv.nevada.edu>
2025-10-25 18:38:55 -0700

    Add files via upload
    initial py source code

---
+++ numINT.py
@@ -0,0 +1,32 @@
+# Some logic adapted from PhoneInfoga by sundowndev
+# Source: https://github.com/sundowndev/phoneinfoga
+# License: GPL 3.0
+import pyfiglet
+import datetime
+import colorama 
+from colorama import Fore, Style
+
+def display():
+    ascii_art = pyfiglet.figlet_format("NumINT", font="slant")
+    print('\033[36m')
+    print(ascii_art)
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print('\033[0m')            
+
+
+if __name__ == "__main__":
+    display()
+
+
+import argparse
+import json
+import phonenumbers
+import requests
+import phonenumbers
+from phonenumbers import geocoder, carrier
+
+parser = argparse.ArgumentParser(description='Phone number intelligence, powered by open source data\nhttps://github.com/T-Devs0/numINT.git', prog='numINT features', usage='%(prog)s -n <number>')
+parser.add_argument('--n', metavar ='number', type=str, help='Enter number to scan here')
+args = parser.parse_args()
+print(parser.description)
+parser.print_usage()
